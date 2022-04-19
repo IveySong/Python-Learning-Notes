a7eb07c91095b609f2ad7a113327c3dca1f1dd7c
Ivey Song <Iveysong@gmail.com>
2022-04-19 09:16:38 +0800

    Delete ifelse.py
    delete it

--- Python-Learning-scripts/ifelse.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-score=int(input("please input a number"))
-if score>=60:
-    if score>=85:
-        print("you are the best")
-    else:
-    	print("you need to fight more")
-else:
-	print("you need to fight hard absolutely")
