8c80d9312a35881bdfb6c1e3163880a28fccfb00
Ivey Song <Iveysong@gmail.com>
2022-04-29 14:10:04 +0800

    Update ch10.2.py

--- Python-Learning-scripts/ch10.2.py
+++ Python-Learning-scripts/ch10.2.py
@@ -10,7 +10,21 @@ try:
 	print('{0}/{1}=={2}'.format(n,i,result))
 except ZeroDivisionError as e:
 	print('it can not be 0, error:{}'.format(e))
+except ValueError as e:
+	# 输入的是无效的数字
+	print('non-valid number:{}'.format(e))
 
 
+#%% 10.2.3 多重异常捕获
+# 将多个except 一起处理就是多重异常捕获
 
-#%% 10.2.2多个except 代码块
+i=input('please input a number:')
+n=888
+
+try:
+	result=n/int(i)
+	print(result)
+	print('{0}/{1}=={2}'.format(n,i,result))
+except (ZeroDivisionError,ValueError) as e:
+	print('Error happen: {}'.format(e))
+
