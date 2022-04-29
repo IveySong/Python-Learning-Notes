c0f10d96d24a610e170dbd518ec63e8e52c5d8b5
IveySong <iveysong@gmail.com>
2022-04-29 22:34:21 +0800

    Create ch11.2.4.py

---
+++ Python-Learning-scripts/ch11.2.4.py
@@ -0,0 +1,23 @@
+# coding = utf-8
+
+import datetime
+
+d=datetime.date.today()
+
+delta=datetime.timedelta(10)
+d+=delta
+
+
+delta=datetime.timedelta(weeks=1)
+d-=delta
+
+
+
+
+#%% 11.2.5 日期与字符串转换
+# 日期转字符串 strftime(format)   实例方法
+# 字符串转日期 strptime(date_string,format)   类方法
+ 
+
+
+
