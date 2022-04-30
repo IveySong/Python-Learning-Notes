eba858e90028c3329ad962f0ed3f9b52a63a0972
IveySong <iveysong@gmail.com>
2022-04-30 16:04:25 +0800

    Create ch12.5 read binary.py

---
+++ Python-Learning-scripts/ch12.5 read binary.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+read(size=-1)   # 读取字节 size=-1 是读取全部字节
+readline(size=-1)  # 从文件中读取并返回一行
+readlines()   #读取文件到一个字节列表中，每一行是列表的一个元素
+
+write(b)  #写入b字节，返回写入的字节数
+writelines(lines) #向文件中写入一个字节列表，不添加行分隔符
+
+flush()  # 写入缓冲区
+
+
+
+
+
+
+
