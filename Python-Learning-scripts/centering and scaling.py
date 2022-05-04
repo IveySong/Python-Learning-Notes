3fddca14f3327968d41ae79d6855fe863c731078
IveySong <iveysong@gmail.com>
2022-05-04 20:54:30 +0800

    Create centering and scaling.py

---
+++ Python-Learning-scripts/centering and scaling.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+import math
+import copy
+import numpy as np
+import pandas as pd
+import os
+from s.impute import SimpleImputer 
+
+
+mtcars=pd.read_csv('E:/MY_Github/Python-Learning-Notes/DataSet/mtcars.csv')
+mtcars.rename(columns={'Unnamed: 0':'model'}, inplace=True)
+
+
+mtcars.index=mtcars.model
+del mtcars['model']
+mtcars.head(4)
