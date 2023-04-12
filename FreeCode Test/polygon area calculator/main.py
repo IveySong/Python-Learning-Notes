10e3574f5a7f2bbdccf0b2bdd3cc25f8a438d945
IveySong <iveysong@gmail.com>
2023-04-12 23:06:29 +0800

    polygon area calculator

---
+++ FreeCode Test/polygon area calculator/main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 23:02:01 2023
+
+@author: Aixia
+"""
+
+import shape_calculator
+from unittest import main
+
+
+rect = shape_calculator.Rectangle(5, 10)
+print(rect.get_area())
+rect.set_width(3)
+print(rect.get_perimeter())
+print(rect)
+
+# sq = shape_calculator.Square(9)
+# print(sq.get_area())
+# sq.set_side(4)
+# print(sq.get_diagonal())
+# print(sq)
+
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
