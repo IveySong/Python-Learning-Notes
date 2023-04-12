456407f59652e45620ad6bb2fed55b72d428a7e9
IveySong <iveysong@gmail.com>
2023-04-12 12:02:20 +0800

    arithmetic formatter

---
+++ FreeCode Test/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 10 22:48:50 2023
+
+@author: Aixia
+"""
+
+from arithmetic_arranger import arithmetic_arranger
+from unittest import main
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+main(module='test_module', exit=False)
+
+
