fc58dce4e19d9d5cf26c163bea8af909a9d2bbde
IveySong <iveysong@gmail.com>
2023-04-12 22:41:32 +0800

    time calculator

---
+++ FreeCode Test/time_calculator/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 22:07:10 2023
+
+@author: Aixia
+"""
+
+
+from time_calculator import add_time
+from unittest import main
+
+print(add_time("11:06 PM", "2:02"))
+
+main(module='test_module', exit=False)
